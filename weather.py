ad210c41a234e503ed12a4a4ad171b9ec430e988
paradime <brw4824@rit.edu>
2016-11-10 11:14:22 -0500

    add weather module

---
+++ weather.py
@@ -0,0 +1,34 @@
+import pyowm
+api = '3501d9bd0853dd3f32f9cd87308c5d45'
+location = '14623,us'
+units = 'fahrenheit'
+
+class Weather:
+    def __init__(self):
+        self.owm = pyowm.OWM(api)
+        self.location = location
+        self.units = units
+
+    def get_temperature(self):
+        obs = self.owm.weather_at_place(self.location)
+        w = obs.get_weather()
+        return w.get_temperature(self.units)['temp']
+
+    def get_weather(self):
+        fc = self.owm.daily_forecast(self.location, limit=1)
+        f = fc.get_forecast()
+        return f.get_weathers()[0]
+
+    def get_high(self):
+        return self.get_weather().get_temperature(self.units)['max'] 
+
+    def get_low(self):
+        return self.get_weather().get_temperature(self.units)['min'] 
+
+    def get_status(self):
+        return self.get_weather().get_detailed_status()
+
+    def get_info(self):
+        report = str.format("The current temperature is {} degrees Fahrenheit. The forecast for today is {}, with a high of {}, and a low of {}.", self.get_temperature(), self.get_status(), self.get_high(), self.get_low())
+        return report
+
