ebee56cf7231685c78a0f6b3e24c540973b457ce
Shayde Nofziger <sln7755@rit.edu>
2016-11-10 09:32:09 -0500

    Stubbed out Alarm class

---
+++ Alarm.py
@@ -0,0 +1,27 @@
+config_file = "example_config.json"
+
+class Alarm:
+    alarm_time = ""
+    is_on = False
+
+    def __init__(self):
+        self.is_on = False
+
+
+    def set_alarm(new_time):
+        self.alarm_time = new_time
+    
+
+    def turn_off():
+        self.is_on = False
+
+
+    def start_alarm():
+        self.is_on = True
+
+
+def main(config_file):
+    pass
+
+
+main(config_file)
