065869a3cff7c90b9a4142b2ed1f7535b820c669
Shayde Nofziger <sln7755@rit.edu>
2016-11-16 19:12:33 -0500

    remove unnecessary functions

--- Alarm.py
+++ Alarm.py
@@ -5,21 +5,14 @@ import json
 class Alarm:
 
     def __init__(self, config):
-        self.is_on = False
         alarm_string = config.alarm["alarmtime"]
         self.alarm_time = datetime.strptime(alarm_string, '%I:%M%p')
 
     def set_alarm(new_time):
         self.alarm_time = new_time
     
-
-    def turn_off():
-        self.is_on = False
-
-
     def start_alarm():
-        self.is_on = True
-
+        pass
 
 def main(config_file):
     config = Configuration(config_file)
