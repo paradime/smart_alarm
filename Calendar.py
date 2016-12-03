01eb914a3e62e152d25c67e3f81bdb5665121f9b
Shayde Nofziger <sln7755@rit.edu>
2016-12-03 10:35:07 -0500

    Return readable string from get_info method

--- Calendar.py
+++ Calendar.py
@@ -23,10 +23,8 @@ CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Python Alarm App'
 
 class Calendar:
-    def __init__(self, config):
-        self.events = get_events()
 
-    def get_credentials():
+    def get_credentials(self):
         """Gets valid user credentials from storage.
 
         If nothing has been stored, or if the stored credentials are invalid,
@@ -54,8 +52,9 @@ class Calendar:
             print('Storing credentials to ' + credential_path)
         return credentials
 
-    def get_events():
-        credentials = get_credentials()
+    # Retrieve events from your calendar using the Google API
+    def get_events(self):
+        credentials = self.get_credentials()
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('calendar', 'v3', http=http)
 
@@ -63,7 +62,6 @@ class Calendar:
         now = tmin.isoformat() + 'Z' # 'Z' indicates UTC time
         tmax = datetime.datetime(year=tmin.year, month=tmin.month, day=tmin.day + 1)
         tmax = tmax.isoformat() + 'Z' # 'Z' indicated UTC time
-        print('Getting the upcoming events for today')
         eventsResult = service.events().list(
             calendarId='primary', timeMin=now, timeMax=tmax, singleEvents=True,
             orderBy='startTime').execute()
@@ -73,6 +71,15 @@ class Calendar:
             return []
         event_starts = []
         for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            event_starts.append(event['summary'] + " " + str(start))
+            start_time = event['start'].get('dateTime', event['start'].get('date'))
+            local_time, offset_time = start_time[:-6], start_time[-6:]
+            offset_time = offset_time.replace(':', '')
+            event_time_formatted = datetime.datetime.strptime(local_time + offset_time, '%Y-%m-%dT%H:%M:%S%z')
+            info_string = event_time_formatted.strftime("%I:%M %p")
+            event_starts.append(event['summary'] + " beginning at " + info_string)
         return event_starts
+
+    def get_info(self):
+        events = self.get_events()
+        report = ". ".join(events)
+        return report
