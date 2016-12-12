adb01049b7f612174ddc2ff8f117aa94428c2e09
paradime <brw4824@rit.edu>
2016-12-12 11:20:55 -0500

    added calendar comments

--- Calendar.py
+++ Calendar.py
@@ -52,42 +52,52 @@ class Calendar:
             print('Storing credentials to ' + credential_path)
         return credentials
 
+    # Gets all events for the day
     def get_events(self):
+        # Uses OAuth. Retreive credentials from the API service or file system.
         credentials = self.get_credentials()
+
+        # Init the API service. 
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('calendar', 'v3', http=http)
-
+        # Format the time correctly to retrieve all events for the current date.
         tmin = datetime.datetime.utcnow()
         now = tmin.isoformat() + 'Z' # 'Z' indicates UTC time
         tmax = datetime.datetime(year=tmin.year, month=tmin.month, day=tmin.day + 1, hour=5)
         tmax = tmax.isoformat() + 'Z' # 'Z' indicated UTC time
+
+        # Retrieve each event from the service and add it to the results list.
         eventsResult = service.events().list(
             calendarId='primary', timeMin=now, timeMax=tmax, singleEvents=True,
             orderBy='startTime').execute()
         events = eventsResult.get('items', [])
 
+        # If there are no events, return an empty list. If they exist, add the
+        # start times and descriptions to the list
         if not events:
             return []
         event_starts = []
-        for event in events:
+        for event in events: # format all events and return them
             event_starts.append(self.format_event(event))
         return event_starts
 
+    # Accepts an event and outputs an event in the form of
+    # [EVENT NAME] beginning at [TIME OF EVENT].
     def format_event(self, event):
             start_time = event['start'].get('dateTime', event['start'].get('date'))
             local_time, offset_time = start_time[:-6], start_time[-6:]
             offset_time = offset_time.replace(':', '')
-            try:
+            try: # Some events that are all day have problems being parsed
                 event_time_formatted = datetime.datetime.strptime(local_time + offset_time, '%Y-%m-%dT%H:%M:%S%z')
             except ValueError:
-                return (event['summary'] + ' all day')
-
+                return (event['summary'] + ' all day') # Catch the all day events
             info_string = ''
             for time in event_time_formatted.strftime("%I:%M %p").split(':'):
-                info_string += time.lstrip('0')
+                info_string += time.lstrip('0') + ' ' # Convert 08:00 pm to 8 pm
             return (event['summary'] + " beginning at " + info_string)
         
 
+    # Pull all events and return a string interpretation of them
     def get_info(self):
         events = self.get_events()
         report = "Your events for today are."
